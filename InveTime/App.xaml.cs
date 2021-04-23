ca47cf23cf7b789b89eea9b66d9a465cf52113e9
Артём Кондратьев <exitway12@gmail.com>
2021-04-23 14:03:03 +0300

    Добавьте файлы проекта.

---
+++ InveTime/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace InveTime
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ InveTime/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace InveTime
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
