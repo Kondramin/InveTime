5745d59c5c16c12e559a322d3a5286e6941d604e
Артём Кондратьев <exitway12@gmail.com>
2021-04-26 13:22:57 +0300

    Added Repositoryes to work with database

--- InveTime.DataBase.DLL/Entityes/Base/Entity.cs
+++ InveTime.DataBase.DLL/Entityes/Base/Entity.cs
@@ -1,6 +1,8 @@
-﻿namespace InveTime.DataBase.DLL.Entityes.Base
+﻿using InveTime.Interfaces;
+
+namespace InveTime.DataBase.DLL.Entityes.Base
 {
-    public abstract class Entity
+    public abstract class Entity : IEntity
     {
         public int Id { get; set; }
     }
---
+++ InveTime.Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace InveTime.Interfaces
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
