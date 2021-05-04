f86336072d5c88291e784506aaca2e92d40a8fdb
Артём Кондратьев <exitway12@gmail.com>
2021-05-04 12:09:58 +0300

    Done with parser

---
+++ TestConsele/ExeleParser.cs
@@ -0,0 +1,66 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsele
+{
+    public class ExeleParser
+    {
+        public static DataTable GetDataFromExcel(string path)
+        {
+
+            var dt = new DataTable();
+
+
+            using (var workBook = new XLWorkbook(path))
+            {
+                var workSheet = workBook.Worksheet(1);
+
+                //Loop through the Worksheet rows.
+                bool firstRow = true;
+                foreach (IXLRow row in workSheet.Rows())
+                {
+                    //Use the first row to add columns to DataTable.
+                    if (firstRow)
+                    {
+                        foreach (IXLCell cell in row.Cells())
+                        {
+                            if (!string.IsNullOrEmpty(cell.Value.ToString()))
+                            {
+                                dt.Columns.Add(cell.Value.ToString());
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        firstRow = false;
+                    }
+                    else
+                    {
+                        int i = 0;
+                        DataRow toInsert = dt.NewRow();
+                        foreach (IXLCell cell in row.Cells(1, dt.Columns.Count))
+                        {
+                            try
+                            {
+                                toInsert[i] = cell.Value.ToString();
+                            }
+                            catch (Exception ex)
+                            {
+
+                            }
+                            i++;
+                        }
+                        dt.Rows.Add(toInsert);
+                    }
+                }
+                return dt;
+            }
+        }
+    }
+}
---
+++ TestConsele/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace TestConsele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Study\TestList.xlsx";
+            DataTable dt = ExeleParser.GetDataFromExcel(path);
+
+
+
+            foreach (DataColumn column in dt.Columns)
+                Console.Write("\t{0}", column.ColumnName);
+            Console.WriteLine();
+            // перебор всех строк таблицы
+            foreach (DataRow row in dt.Rows)
+            {
+                // получаем все ячейки строки
+                var cells = row.ItemArray;
+                foreach (object cell in cells)
+                    Console.Write("\t{0}", cell);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
